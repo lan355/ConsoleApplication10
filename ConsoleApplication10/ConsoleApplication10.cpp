8b6db0b9c0e8b9dcee3059d9e76faf0bb3f2de85
cfif bdfyjd <lan467@mail.ru>
2019-06-04 21:43:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	string s;
+	cout << "Введите предложение:";
+	getline(cin, s);
+
+	size_t a = s.find(' ');
+	size_t b = s.rfind(' ');
+
+	string w1 = s.substr(0, a);
+	string w2 = s.substr(b + 1);
+
+	s.replace(b + 1, s.length() - b, w1);
+	s.replace(0, a, w2);
+
+	cout << s << endl;
+}
+
+
